500f6e6cf0814c7d6a5f3c52c30a4924a4228988
NwobiaDavid <dnwobia@gmail.com>
2024-01-02 12:10:56 +0100

    built out the left side bar and the top bar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,6 +13,22 @@ export default function Topbar() {
         </p>
       </Link>
 
+      <div className="flex items-center gap-1 ">
+        <div className="block md:hidden ">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer ">
+                <Image width={24} height={24}  alt="logout" src="/assets/logout.svg" />
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher appearance={{
+          elements: {
+              organizationSwitcherTrigger: "py-2 px-4"
+          }
+        }} />
+      </div>
     </nav>
   )
 }
